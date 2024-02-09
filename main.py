1e16ab3039d78dad28fa2486a299af5959f4876a
Бура88 <ariorh88@gmail.com>
2024-02-09 19:16:15 +0300

    Первый вариант

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print ('hhh')
+from art import tprint
+import requests
+from rich import print as rprint
+tprint ("checkhost","amcaaa01")
+rprint ("'создано при проекте *Python для чайников*'")
+
+
+
+url = input('URL: ')
+
+
+def valio(url):
+        try:
+                if 'http://' in url or 'https://' in url:
+                        response = requests.get(url)
+                else:
+                        response = requests.get(f'https://{url}')
+
+                return (str(response.status_code))
+
+        except:
+                print ('ошибка')
+print (valio(url))
